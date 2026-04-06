619b28ddf9493befd3e1d9e81d27087af644879a
bosunogunlana <tunbosunogunlana1@gmail.com>
2026-04-06 14:21:53 -0400

    chore: logging cleanup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,7 +11,10 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		AddSource: true,
+	}))
 
 	mux := http.NewServeMux()
 
